8c56b5c6f6d57811d65f35c910f82013a7311384
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-12-24 11:49:17 +0800

    task 6. backend

--- app/src/main/java/com/khairul/practical9_roompersistence/AddNoteActivity.java
+++ app/src/main/java/com/khairul/practical9_roompersistence/AddNoteActivity.java
@@ -2,13 +2,90 @@ package com.khairul.practical9_roompersistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String ExtraDate="date";
+    public static final String ExtraMood ="mood";
+    public static final String ExtraDayNight ="daynight";
+    public static final String ExtraNote= "note";
+
+    private EditText ETDate, ETNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+
+        //init
+        RadioButton RBHappy= findViewById(R.id.RBHappy);
+        RadioButton RBNeutral = findViewById(R.id.RBNeutral);
+        RadioButton RBUnhappy = findViewById(R.id.RBUnhappy);
+
+        RadioButton RBDay = findViewById(R.id.RBDay);
+        RadioButton RBNight= findViewById(R.id.RBNight);
+
+        //
+        ETDate = findViewById(R.id.ETDate);
+        ETNote = findViewById(R.id.ETNote);
+
+        final Button BtnSave= findViewById(R.id.BtnSave);
+
+        BtnSave.setOnClickListener(view->{
+
+            Intent replyIntent= new Intent();
+
+            String date= ETDate.getText().toString();
+            String note= ETNote.getText().toString();
+
+            //mood
+            int mood= 2;
+
+            if (RBHappy.isChecked()){
+                mood =1;
+            }
+            else if (RBNeutral.isChecked()){
+                mood =2;
+            }
+            else {
+                mood=3;
+            }
+
+            //daynight
+            boolean dayNight= true;
+
+            if (RBDay.isChecked()){
+                dayNight= true;
+            }
+            else {
+                dayNight= false;
+            }
+
+            //text
+            if (TextUtils.isEmpty(ETDate.getText())){
+                setResult(RESULT_CANCELED, replyIntent);
+            }
+            else if (TextUtils.isEmpty(ETNote.getText()) ){
+                setResult(RESULT_CANCELED, replyIntent);
+            }
+            else {
+                replyIntent.putExtra(ExtraDate, date);
+                replyIntent.putExtra(ExtraNote, note);
+
+                replyIntent.putExtra(ExtraMood, Integer.toString(mood));
+                replyIntent.putExtra(ExtraDayNight, Boolean.toString(dayNight));
+
+                setResult(RESULT_OK, replyIntent);
+            }
+            finish();
+        });
+
+
     }
 }
--- app/src/main/java/com/khairul/practical9_roompersistence/MainActivity.java
+++ app/src/main/java/com/khairul/practical9_roompersistence/MainActivity.java
@@ -1,14 +1,82 @@
 package com.khairul.practical9_roompersistence;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.TokenWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NEW_NOTE_REQUEST_CODE=1;
+
+    private MoodNoteViewModel moodNoteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init
+        RecyclerView RVMoodNote= findViewById(R.id.RVMoodNote);
+
+        final MoodNoteListAdapter adapter= new MoodNoteListAdapter( new MoodNoteListAdapter.NoteDiff());
+
+        RVMoodNote.setAdapter(adapter);
+        RVMoodNote.setLayoutManager(new LinearLayoutManager(this));
+
+        moodNoteViewModel = new ViewModelProvider(this).get(MoodNoteViewModel.class);
+
+        moodNoteViewModel.getAllNotes().observe(this, notes->{
+            adapter.submitList(notes);
+        });
+
+        FloatingActionButton FABAddNote= findViewById(R.id.FABAddNote);
+
+        FABAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+
+                startActivityForResult(intent, NEW_NOTE_REQUEST_CODE);
+            }
+        });
+
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode== NEW_NOTE_REQUEST_CODE && resultCode == RESULT_OK){
+
+            String date= data.getStringExtra( AddNoteActivity.ExtraDate);
+
+            int mood= Integer.parseInt( data.getStringExtra( AddNoteActivity.ExtraMood));
+
+            boolean dayNight= Boolean.parseBoolean( data.getStringExtra( AddNoteActivity.ExtraDayNight));
+
+            String mNote= data.getStringExtra( AddNoteActivity.ExtraNote);
+
+            MoodNote note = new MoodNote(date, mood, dayNight, mNote);
+            moodNoteViewModel.insert(note);
+
+
+        } //if requestcode
+        
+        else {
+            Toast.makeText(getApplicationContext(), R.string.empty_not_saved, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
 }
